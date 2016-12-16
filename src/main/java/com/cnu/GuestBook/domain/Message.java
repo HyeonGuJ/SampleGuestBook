17025be1bb6dd668dd89bf91195247d70b1394f5
HyeonGu Jeon <jhg3182@naver.com>
2016-12-16 16:02:37 +0900

    config root-context.xml / add files for mybatis

---
+++ src/main/java/com/cnu/GuestBook/domain/Message.java
@@ -0,0 +1,48 @@
+package com.cnu.GuestBook.domain;
+
+import java.util.Date;
+
+public class Message {
+	private int id;
+	private String email;
+	private String password;
+	private Date date;
+	private String text;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", email=" + email + ", password=" + password + ", date=" + date + ", text=" + text
+				+ "]";
+	}	
+	
+	
+}
---
+++ src/main/java/com/cnu/GuestBook/persistence/MassageDAO.java
@@ -0,0 +1,5 @@
+package com.cnu.GuestBook.persistence;
+
+public class MassageDAO {
+
+}
