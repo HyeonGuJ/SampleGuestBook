2ab328029da272671d4df2cd783a7d2c4750bd04
JHG <JHG@14.50.164.129>
2016-12-31 21:23:30 +0900

    update messageVO

--- src/main/java/com/cnu/GuestBook/MessageVO.java
+++ src/main/java/com/cnu/GuestBook/MessageVO.java
@@ -1,8 +1,5 @@
 package com.cnu.GuestBook;
 
-import lombok.Data;
-
-@Data
 public class MessageVO {
 	
 	private int idMessage;
@@ -11,6 +8,79 @@ public class MessageVO {
 	private String date;
 	private String ModifiedDate;
 	private String text;
+
+	
+
+	public int getIdMessage() {
+		return idMessage;
+	}
+
+
+
+	public void setIdMessage(int idMessage) {
+		this.idMessage = idMessage;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+
+	public String getModifiedDate() {
+		return ModifiedDate;
+	}
+
+
+
+	public void setModifiedDate(String modifiedDate) {
+		ModifiedDate = modifiedDate;
+	}
+
+
+
+	public String getText() {
+		return text;
+	}
+
+
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
 
 
 	@Override
